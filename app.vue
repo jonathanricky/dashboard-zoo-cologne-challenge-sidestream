0238e2a3f6761643b430929c24cd53c87510f9ac
jonathanricky <64757579+jonathanricky@users.noreply.github.com>
2023-09-07 21:07:56 +0200

    🍞 UI fix

--- app.vue
+++ app.vue
@@ -12,12 +12,14 @@ const animals: Array<Animal> = await $fetch('/api/animals')
     <h1 class="text-4xl">
       Cologne Zoo🦁
     </h1>
+    <br>
     <div class="my-2">
       <p>
         This is the overview board of the Cologne Zoo. It can be used by zookeepers to get an overview of the animals and their relevant health data.
       </p>
+      <br>
       <p>
-        Please contact the zoo admin at "admin [at] zoo-cologne [dot] de" for support with the dashboard. Cyber-Attacks from the Duisburg Zoo have become more frequent on the past, so be on the lookout.
+        Please contact the zoo admin at "admin@zoo-cologne.de" for support with the dashboard. Cyber-Attacks from the Duisburg Zoo have become more frequent on the past, so be on the lookout.
       </p>
     </div>
     <h2 class="text-2xl mt-8 mb-4">
